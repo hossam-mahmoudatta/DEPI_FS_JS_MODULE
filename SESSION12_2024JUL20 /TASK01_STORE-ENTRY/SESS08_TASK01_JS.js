d4bd0a3692d8d2324979801b5655e95a0e07f529
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-07-20 19:39:33 +0300

    added some code

--- SESSION12_2024JUL20 /TASK01_STORE-ENTRY/SESS08_TASK01_JS.js
+++ SESSION12_2024JUL20 /TASK01_STORE-ENTRY/SESS08_TASK01_JS.js
@@ -1,55 +1,13 @@
+var merchants = [];
+var products = [];
 
-.item {
-    background-color: aqua;
-    height: 50px;
-    width: 50px;
-    border-radius: 50%;
-    position: relative;
-    animation-name: background-changer;
-    animation-duration: 10s;
-    animation-iteration-count: 10;
-    /* animation-delay: 5s; */
-    /* animation-timing-function: ease-out; */
-    /* animation: moveHorizontally  3s linear infinite; */
+function addProduct () {
+    var Mo;
 }
 
-@keyframes background-changer {
-    0% {
-        background-color: yellow;
-        border-radius: 12%;
-        left: 0%;
-    }
-    
-    25% {
-        background-color: blue;
-        border-radius: 24%;
-        left: 50%;
-    }
-    
-    50% {
-        background-color: pink;
-        border-radius: 36%;
-        left: 100%;
-    }
-
-    75% {
-        background-color: pink;
-        border-radius: 36%;
-        left: 50%;
-    }
-
-    100% {
-        background-color: blueviolet;
-        border-radius: 50%;
-        left: 0%;
-    }
+function addMerchant () {
+    var sho;
 }
 
-/* @keyframes background-changer {
-    from {
-        background-color: yellow;
-    }
-    to {
-        background-color: red;
-    }
-} */
+
+// I want to map specfic products to each merchant
