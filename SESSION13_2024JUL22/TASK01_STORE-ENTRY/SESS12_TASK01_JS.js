bfa371f6b61f37e6872041e069a853dcc20fd6ba
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-07-27 16:50:49 +0300

    added session folders

---
+++ SESSION13_2024JUL22/TASK01_STORE-ENTRY/SESS12_TASK01_JS.js
@@ -0,0 +1,13 @@
+var merchants = [];
+var products = [];
+
+function addProduct () {
+    var Mo;
+}
+
+function addMerchant () {
+    var sho;
+}
+
+
+// I want to map specfic products to each merchant
