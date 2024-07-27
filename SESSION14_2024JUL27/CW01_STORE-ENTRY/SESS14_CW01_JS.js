688bbcddb71a462b65282097ca076cd39b1e3b28
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-07-27 19:18:40 +0300

    adding the classwork

---
+++ SESSION14_2024JUL27/CW01_STORE-ENTRY/SESS14_CW01_JS.js
@@ -0,0 +1,10 @@
+
+const title = document.getElementById('headerHosa');
+const title2 = document.getElementsByClassName('headerTwo');
+const title3 = document.querySelector('.one');
+
+console.log(title);
+console.log(title2);
+console.log(title3);
+
+// I want to map specific products to each merchant
