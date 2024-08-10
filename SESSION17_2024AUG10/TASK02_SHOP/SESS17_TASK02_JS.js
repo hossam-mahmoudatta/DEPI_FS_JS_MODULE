d7d4caa122cb8850180daa2e939a4c5fac135fd4
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-08-10 19:15:25 +0300

    renamed some files

--- SESSION17_2024AUG10/TASK02_SHOP/SESS17_TASK02_JS.js
+++ SESSION17_2024AUG10/TASK02_SHOP/SESS17_TASK02_JS.js
@@ -67,3 +67,19 @@ generateButton.addEventListener("click",  () => {
 //     randomDiv.textContent = `${quotes.name} is ${quotes.quote} years old.`;
 //     quoteDiv.appendChild(randomDiv);
 // });
+
+let userInputString = prompt("Please enter your input: ");
+console.log("User input is:", userInputString);
+
+const words = userInputString.split(" ");
+let largestWord = words[0];
+let largestWordLength = words[0].length;
+
+for (let i = 1 ; i < words.length ; i++) {
+    if (words[i].length > largestWordLength) {
+        largestWord = words[i];
+        largestWordLength = words[i].length;
+    }
+}
+
+console.log('The word with the maximum length is ' + largestWord + ' and its length is ' + largestWordLength);
